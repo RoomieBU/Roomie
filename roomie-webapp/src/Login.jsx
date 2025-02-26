df6c4ad78dda897b531e2b3b428d99ca8fbe35de
Onlyriley <90656278+Onlyriley@users.noreply.github.com>
2025-02-25 22:44:23 -0500

    Routers and Controllers
    Logic for the routes has been moved to a new Router class, and logic for authentication has been moved to AuthController.

--- roomie-webapp/src/Login.jsx
+++ roomie-webapp/src/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
 
     const onSubmit = async (data) => {
         try {
-            const response = await fetch("http://18.116.38.166:8080/auth/login", {
+            const response = await fetch("http://roomie.ddns.net:8080/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
