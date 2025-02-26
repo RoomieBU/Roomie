66c1454e1ca9093eb1fc9bcbf58b075f5e0a1253
SalvatoreLaMarca1 <148724040+SalvatoreLaMarca1@users.noreply.github.com>
2025-02-25 22:15:20 -0500

    Work done on Matching page

---
+++ roomie-webapp/src/Matching.jsx
@@ -0,0 +1,35 @@
+import "./Matching.css"
+
+function Matching() {
+
+
+
+    return (
+        <div className="hold-all">
+            <div className="potential-roomate">
+
+                <div className="user_info">
+                    <text id="name">name</text>
+                    <text>, </text>
+                    <text id="age">age</text>
+                    <br/>
+                    <text id="university">university</text>
+                </div>
+
+            </div> 
+            
+            <div className="match-button-cluster">
+                <button>
+                    X
+                </button>
+                    
+                <button>
+                    M
+                </button>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Matching
