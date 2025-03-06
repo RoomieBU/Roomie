fea14525529f73a0df424b1917d31d01b847401e
samkapp <samkapp.sk@gmail.com>
2025-03-06 18:02:50 -0500

    updated app.jsx

--- roomie-webapp/src/App.jsx
+++ roomie-webapp/src/App.jsx
@@ -22,7 +22,6 @@ function App() {
         <Route path="/matching" element={<Matching />}/>
         <Route path="/preferences" element={<Preferences />}/>
         <Route path = "/fileSubmit" element={<FileSubmit />} />
-        <Route path = "/user/images" element{<TempImageDemo />} >
         <Route path="*" element={<Navigate to="/" replace />} />  {/* Redirect unknown routes */}
       </Routes>
     </Router>
