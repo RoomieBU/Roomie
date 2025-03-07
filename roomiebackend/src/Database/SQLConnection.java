afe85d877ecab5b5a29f01f330f990c5d0888aed
Onlyriley <90656278+Onlyriley@users.noreply.github.com>
2025-03-07 11:38:51 -0500

    Update SQLConnection.java

--- roomiebackend/src/Database/SQLConnection.java
+++ roomiebackend/src/Database/SQLConnection.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class SQLConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/Roomie";
     private static final String USER = "database";
-    private static final String PASSWORD = "Roomie";
+    private static final String PASSWORD = System.getenv("SSL_KEY");
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
