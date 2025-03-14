59a2463c8d3ddb20f501f616d8f1b5dd024b1f32
Onlyriley <90656278+Onlyriley@users.noreply.github.com>
2025-03-14 11:26:30 -0400

    Update MatchController.java

--- roomiebackend/src/MatchController.java
+++ roomiebackend/src/MatchController.java
@@ -1,7 +1,5 @@
-import Database.SQLConnection;
-import Database.User;
-import Database.UserDao;
-import Database.UserMatchInteractionDao;
+import Database.*;
+
 import java.sql.SQLException;
 import java.util.*;
 
@@ -31,9 +29,8 @@ public class MatchController {
 
 
         try {
-            UserMatchInteractionDao DBMatch = new UserMatchInteractionDao(SQLConnection.getConnection());
-
-            if(DBMatch.sendMatchInteraction(matchData, email)) {
+            Dao dao = new Dao(SQLConnection.getConnection());
+            if(dao.insert(matchData, "UserMatchInteractions")) {
                 response.put("message", "Send Match Interaction data for " + email);
                 code = 200;
             } else {
@@ -66,6 +63,8 @@ public class MatchController {
 
 
         try {
+            // This must be DBUser to get list of users
+            // ** Don't change this to dao class
             UserDao DBUser = new UserDao(SQLConnection.getConnection());
 
             // Get the current users school
@@ -77,14 +76,13 @@ public class MatchController {
 
 
             // get a list of all the users, where the user has the same school as the
-            // currently logged in user
             List<User> users = DBUser.getAllUsersBySchool(school);
             User user;
             // Just got now get a random user (does no checking if it's the same user)
             Random rand = new Random();
             do {
                 user = users.get(rand.nextInt(0, users.size() - 1));
-            } while (!user.getRegistered() || user.getDateOfBirth() == null);
+            } while (!user.getRegistered() || user.getDateOfBirth() == null || user.getEmail().equals(email));
 
             if (users.get(0) != null) {
                 response.put("message", "Next match found");
