c493f7bf504f1bcb2ae2d2d206995c2b5f956fa9
Emily Faso <153043260+efaso87@users.noreply.github.com>
2025-03-30 15:24:39 -0400

    Updating the Front-End for the Registration Page

--- roomie-webapp/src/Registration.jsx
+++ roomie-webapp/src/Registration.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import "./Registration.css"; // Import the CSS file
 
 function Registration() {
     const navigate = useNavigate();
@@ -62,87 +63,87 @@ function Registration() {
     };
 
     return (
-        <div className="manBun">
-            <div className="container d-flex flex-column align-items-center justify-content-center">
-                <h1 className="fw-bold">Register for ROOMIE</h1>
-                <p>
+        <div className="registration-container">
+            <div className="registration-card">
+                <h1 className="registration-title">Register for ROOMIE.</h1>
+                <p className="registration-subtext">
                     Already have an account?{" "}
-                    <a href="" onClick={() => navigate("/login")}>
+                    <a href="" onClick={() => navigate("/login")} className="registration-link">
                         Sign in!
                     </a>
                 </p>
-                <form onSubmit={handleSubmit(onSubmit)} className="w-50">
-                    <div className="mb-3">
-                        <label className="form-label">First Name</label>
+                <form onSubmit={handleSubmit(onSubmit)} className="registration-form">
+                    <div className="form-group">
+                        <label className="form-label">First Name:</label>
                         <input
                             type="text"
                             className={`form-control ${errors.first_name ? "is-invalid" : ""}`}
-                            {...register("first_name", { required: "First name is required" })}
+                            {...register("first_name", { required: "First name is required." })}
                         />
                         {errors.first_name && <div className="invalid-feedback">{errors.first_name.message}</div>}
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Last Name</label>
+                    <div className="form-group">
+                        <label className="form-label">Last Name:</label>
                         <input
                             type="text"
                             className={`form-control ${errors.last_name ? "is-invalid" : ""}`}
-                            {...register("last_name", { required: "Last name is required" })}
+                            {...register("last_name", { required: "Last name is required." })}
                         />
                         {errors.last_name && <div className="invalid-feedback">{errors.last_name.message}</div>}
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">About Me</label>
+                    <div className="form-group">
+                        <label className="form-label">A Little Bit About Yourself:</label>
                         <textarea
                             className={`form-control ${errors.about_me ? "is-invalid" : ""}`}
-                            {...register("about_me", { required: "Please write something about yourself" })}
+                            {...register("about_me", { required: "Please write something about yourself." })}
                         />
                         {errors.about_me && <div className="invalid-feedback">{errors.about_me.message}</div>}
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Date of Birth</label>
+                    <div className="form-group">
+                        <label className="form-label">Date of Birth:</label>
                         <input
                             type="date"
                             className={`form-control ${errors.date_of_birth ? "is-invalid" : ""}`}
-                            {...register("date_of_birth", { required: "Date of birth is required" })}
+                            {...register("date_of_birth", { required: "Date of birth is required." })}
                         />
                         {errors.date_of_birth && <div className="invalid-feedback">{errors.date_of_birth.message}</div>}
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Major</label>
+                    <div className="form-group">
+                        <label className="form-label">Major:</label>
                         <input
                             type="text"
                             className={`form-control ${errors.major ? "is-invalid" : ""}`}
-                            {...register("major", { required: "Major is required" })}
+                            {...register("major", { required: "Major is required." })}
                         />
                         {errors.school && <div className="invalid-feedback">{errors.major.message}</div>}
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">School</label>
+                    <div className="form-group">
+                        <label className="form-label">School:</label>
                         <input
                             type="text"
                             className={`form-control ${errors.school ? "is-invalid" : ""}`}
-                            {...register("school", { required: "School is required" })}
+                            {...register("school", { required: "School is required." })}
                         />
                         {errors.school && <div className="invalid-feedback">{errors.school.message}</div>}
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Profile Picture</label>
+                    <div className="form-group">
+                        <label className="form-label">Upload a Profile Picture:</label>
                         <input
                             type="file"
                             accept="image/*"
                             className={`form-control ${errors.major ? "is-invalid" : ""}`}
-                            {...register("photo", { required: "Photo is required" })}
+                            {...register("photo", { required: "Profile Photo is required." })}
                         />
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Email Verification Code</label>
+                    <div className="form-group">
+                        <label className="form-label">Email Verification Code:</label>
                         <input
                             type="text"
                             className={`form-control ${errors.code ? "is-invalid" : ""}`}
@@ -151,9 +152,9 @@ function Registration() {
                         {errors.code && <div className="invalid-feedback">{errors.code.message}</div>}
                     </div>
 
-                    {registrationError && <div className="text-danger mb-3">{registrationError}</div>}
+                    {registrationError && <div className="registration-error">{registrationError}</div>}
 
-                    <button type="submit" className="btn btn-primary w-100">
+                    <button type="submit" className="registration-button">
                         Register
                     </button>
                 </form>
